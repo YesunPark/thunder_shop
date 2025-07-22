ca8089d87f4bafcb17aeece6d8a8de9203d002a9
HyuntaeparkK <atolistyon@gmail.com>
2025-07-22 15:06:41 +0900

    Fix: salePrice가 0이면, 할인처리 하는 UI 안보여주기

--- lib/product_detail/widgets/product_price_info.dart
+++ lib/product_detail/widgets/product_price_info.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ProductPriceInfo extends StatelessWidget {
   final int originalPrice;
-  final int salePrice;
+  final int? salePrice;
 
   const ProductPriceInfo({
     super.key,
@@ -10,41 +11,55 @@ class ProductPriceInfo extends StatelessWidget {
     required this.salePrice,
   });
 
+  String formatPrice(int price) {
+    return NumberFormat('#,###').format(price);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final discount = (100 - salePrice / originalPrice * 100).round();
+    // 할인가가 null, 0, 또는 원가와 같거나 더 크면: 할인 표시 X
+    final bool hasDiscount =
+        salePrice != null && salePrice! > 0 && salePrice! < originalPrice;
+
+    final int displaySalePrice = hasDiscount ? salePrice! : originalPrice;
+    final int discount = hasDiscount
+        ? (100 - (salePrice! / originalPrice * 100)).round()
+        : 0;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          '$originalPrice원',
-          style: const TextStyle(
-            color: Colors.grey,
-            decoration: TextDecoration.lineThrough,
-            fontSize: 14,
+        if (hasDiscount) // 원가 취소선은 할인가 있을 때만
+          Text(
+            '${formatPrice(originalPrice)}원',
+            style: const TextStyle(
+              color: Colors.grey,
+              decoration: TextDecoration.lineThrough,
+              fontSize: 14,
+            ),
           ),
-        ),
-        const SizedBox(height: 2),
+        if (hasDiscount) const SizedBox(height: 2),
         Row(
           children: [
             Text(
-              '$salePrice원',
-              style: const TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
+              '${formatPrice(displaySalePrice)}원',
+              style: TextStyle(
+                color: hasDiscount ? Colors.red : Colors.black,
+                fontWeight: hasDiscount ? FontWeight.bold : FontWeight.normal,
                 fontSize: 18,
               ),
             ),
-            const SizedBox(width: 8),
-            Text(
-              '-$discount%',
-              style: const TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+            if (hasDiscount) ...[
+              const SizedBox(width: 8),
+              Text(
+                '-$discount%',
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
-            ),
+            ],
           ],
         ),
       ],
